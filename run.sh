6b6cfd2e9ca146e572ba442690ebc1e8216213fb
thebrianzeng <thebrianzeng@gmail.com>
2015-09-27 12:28:20 -0400

    update run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker stop marketplace_instance
 sudo docker rm marketplace_instance
-sudo docker run --name marketplace_instance -p 5000:5000 -i -t marketplace_img
+sudo docker run --name marketplace_instance -d -p 5000:5000 -i -t marketplace_img
